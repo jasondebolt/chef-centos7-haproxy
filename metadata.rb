eb1b93d52bc0b75e59ce1bc1934c174120c3c0ce
jasondebolt <jasondebolt@gmail.com>
2017-05-04 14:12:09 -0700

    haproxy chef recipe working with 2 web nodes and a load balancing node

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,13 @@ maintainer_email 'jasondebolt@gmail.com'
 license 'all_rights'
 description 'Installs/Configures chef-centos7-haproxy'
 long_description 'Installs/Configures chef-centos7-haproxy'
-version '0.1.0'
+version '0.1.6'
 
 issues_url 'https://github.com/jasondebolt/chef-centos7-haproxy/issues' if respond_to?(:issues_url)
 
 source_url 'https://github.com/jasondebolt/chef-centos7-haproxy' if respond_to?(:source_url)
 
 depends 'chef-centos7-common'
-depends 'chef-centos7-workstation'
+#depends 'chef-centos7-workstation'
 depends 'chef-centos7-httpd'
+depends 'haproxy', '~> 4.0.0'
